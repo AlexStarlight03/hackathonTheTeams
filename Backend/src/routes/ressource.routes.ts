edc1e0001899b058480b903f222fe45751c4b799
TheAlecx <alexandre.leblanc.99@videotron.ca>
2026-01-14 13:10:10 -0500

    First commit prisma terminer controller et route semi terminer

--- Backend/src/routes/ressource.routes.ts
+++ Backend/src/routes/ressource.routes.ts
@@ -5,6 +5,15 @@ import {
     getAllRessources,
     getRessourceById,
     updateRessource 
-}
+}from '../controllers/ressource.controller';
 
-from '../controllers/ressource.controller';
+const router = Router();
+
+router.post('/ressources', createRessource);
+router.get('/ressources', getAllRessources);
+router.get('/ressources/:id', getRessourceById);
+router.put('/ressources/:id', updateRessource);
+router.delete('/ressources/:id', deleteRessource);
+
+export default router;
+
